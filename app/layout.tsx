6c93cc5259208746022ccdc6026e3bbefe18129a
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-10-29 19:23:22 +0200

    initial setup project

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { fontSans } from '@/lib/fonts';
+
+import { cn } from '@/lib/utils';
+import { siteConfig } from '@/config/site';
+
+export const metadata: Metadata = {
+	title: {
+		default: siteConfig.name,
+		template: `%s - ${siteConfig.name}`,
+	},
+	description: siteConfig.description,
+	keywords: siteConfig.keywords,
+	authors: [
+		{
+			name: 'wael mando',
+			url: 'https://twitter.com/waelmando0',
+		},
+	],
+	creator: 'waelmando',
+	icons: {
+		icon: '/favicon.ico',
+		shortcut: '/favicon-16x16.png',
+		apple: '/apple-touch-icon.png',
+	},
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang='en'>
+			<body
+				className={cn(
+					'min-h-screen bg-background font-sans antialiased',
+					fontSans.variable
+				)}
+			>
+				<div className='relative flex min-h-screen flex-col'>
+					<main className='flex-1'>{children}</main>
+				</div>
+			</body>
+		</html>
+	);
+}
---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: 'Discord Clone',
+	description: 'Discord Clone website built with Shadcn UI and Tailwind  CSS.',
+	keywords: [
+		'Next.js',
+		'React',
+		'TypeScript',
+		'Tailwind CSS',
+		'Server Components',
+		'Radix UI',
+	],
+};
