caace48cbc2d91f378d214507270dec0afacc8eb
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-11-11 10:55:04 +0200

    update style for mode toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,11 +18,13 @@ export function ModeToggle() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant='outline' size='icon'>
-					<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-					<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-					<span className='sr-only'>Toggle theme</span>
-				</Button>
+				<button className='group flex items-center'>
+					<div className='flex items-center justify-center mx-3 h-[48px] w-[48px] rounded-[24px] transition-all overflow-hidden bg-background dark:bg-neutral-700  group-hover:rounded-[16px] group-hover:bg-emerald-500 border'>
+						<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+						<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-emerald-500 group-hover:text-white' />
+						<span className='sr-only'>Toggle theme</span>
+					</div>
+				</button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align='end'>
 				<DropdownMenuItem onClick={() => setTheme('light')}>
